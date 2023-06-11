950e0d15cce2d4bfe99e181a049432426ed1e9b9
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-06-11 21:29:07 +0300

    area()

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,8 +7,25 @@ public class MyFirstProgram {
     hello("world");
     hello("user");
     hello("Irina");
+
+    double l = 5;
+    out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
   }
   public static void hello(String somebody) {
     out.println("Hello, " + somebody + " ! ");
   }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
 }
