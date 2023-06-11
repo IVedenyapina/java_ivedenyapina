b75ec37beeec05adcfd9c7d439d15342a27b3ca1
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-06-11 20:27:15 +0300

    Hello, world!

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,27 +4,6 @@ import static java.lang.System.*;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Alexei");
-
-    double l = 5;
-    out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-
-    double a = 4;
-    double b = 6;
-    out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
-  }
-  public static void hello(String somebody) {
-    System.out.println("Hello " + somebody + "!");
-
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-  public static double area(double a, double b) {
-    return a * b;
-
+    System.out.println("Hello, world!");
   }
 }
