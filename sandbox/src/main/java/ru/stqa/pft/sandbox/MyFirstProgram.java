aab9007e908e49be776e125c30d85cfa29d1449c
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-06-11 20:36:19 +0300

    Hello, Irina!

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,11 @@ import static java.lang.System.*;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Irina");
+  }
+  public static void hello(String somebody) {
+    out.println("Hello, " + somebody + " ! ");
   }
 }
