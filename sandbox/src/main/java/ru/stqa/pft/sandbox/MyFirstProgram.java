dcd64e043902ea72f1058f44799c7f4b165a61eb
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-06-12 11:08:45 +0300

    edit func

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -31,6 +31,6 @@ public class MyFirstProgram {
   }
 
   public static double distance(Point p1, Point p2){
-    return Math.sqrt((p1.x1-p2.x2)*(p1.y1-p2.y2));
+    return Math.sqrt((p2.x2-p1.x1)*(p2.x2-p1.x1) + (p2.y2-p1.y1)*(p2.y2-p1.y1));
   }
 }
