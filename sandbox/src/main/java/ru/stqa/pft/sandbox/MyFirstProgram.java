bf521882d349b8c1ce91eca2a3bd84fba4fbc58b
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-06-12 13:32:23 +0300

    the last commit Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -15,22 +15,23 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
+
+
     Point p1 = new Point();
-    p1.x1 = 0;
-    p1.y1 = 1;
+    p1.x1 = 6;
+    p1.y1 = 2;
 
     Point p2 = new Point();
-    p2.x2 = 2;
-    p2.y2 = -2;
+    p2.x2 = -5;
+    p2.y2 = 1;
 
-    out.println("Расстояние между точками p1 и p2 = " + distance(p1, p2));
+    Point d = new Point();
+
+        out.println("Расстояние между точками p1 и p2 = " + d.distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     out.println("Hello, " + somebody + " ! ");
   }
 
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x2-p1.x1)*(p2.x2-p1.x1) + (p2.y2-p1.y1)*(p2.y2-p1.y1));
-  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,7 +6,10 @@ public class Point {
   public double x2;
   public double y2;
 
+  public double distance(Point p1, Point p2) {
+    double d  = Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1) * (p2.y2 - p1.y1));
+    return d;
+  }
 }
-
 
 
