592a755eda9f6c5154d792a7736f6062a1cf7894
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-06-25 21:39:15 +0300

    rename wd

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
@@ -1,48 +1,44 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-
 import org.testng.annotations.*;
 
 import static org.testng.Assert.*;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class ContactDeletionTests {
-  private WebDriver driver;
+  private WebDriver wd;
   private boolean acceptNextAlert = true;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
+    wd = new FirefoxDriver();
   }
 
   @Test
-  public void testContactDeletionTests() throws Exception {
-    driver.get("http://localhost:8080/addressbook/");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-    driver.findElement(By.name("selected[]")).click();
+  public void testContactDeletion() throws Exception {
+    wd.get("http://localhost:8080/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    wd.findElement(By.name("selected[]")).click();
     acceptNextAlert = true;
-    driver.findElement(By.xpath("//input[@value='Delete']")).click();
+    wd.findElement(By.xpath("//input[@value='Delete']")).click();
     assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    driver.quit();
+    wd.quit();
   }
 
   private boolean isElementPresent(By by) {
     try {
-      driver.findElement(by);
+      wd.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -51,7 +47,7 @@ public class ContactDeletionTests {
 
   private boolean isAlertPresent() {
     try {
-      driver.switchTo().alert();
+      wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
@@ -59,7 +55,7 @@ public class ContactDeletionTests {
   }
   private String closeAlertAndGetItsText() {
     try {
-      Alert alert = driver.switchTo().alert();
+      Alert alert = wd.switchTo().alert();
       String alertText = alert.getText();
       if (acceptNextAlert) {
         alert.accept();
