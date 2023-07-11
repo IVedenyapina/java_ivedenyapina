22c688c574647a4e479f3df09ce0b9525212b693
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-07-11 15:27:31 +0300

    3.10 success

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,6 +7,10 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getContactHelper().returnToHomePage();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getNavigationHelper().gotoAddNewPage();
+      app.getContactHelper().createContact(new ContactData("Ирина", "Веденяпина", "+79273712441", "ivedenyapina@yandex.ru", "Test44"), true);
+    }
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("Мария", "Веденяпина", "+79273713569", "mvedenyapina@yandex.ru", null), false);
