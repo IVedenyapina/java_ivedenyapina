a2a2e26e0c8522a944d0ad2efee2378fdd94a2d0
IVedenyapina <127577705+IVedenyapina@users.noreply.github.com>
2023-07-12 14:57:40 +0300

    Задание 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTestDifferent.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTestDifferent.java
@@ -3,19 +3,12 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static java.awt.geom.Point2D.distance;
-
-public class PointTests {
+public class PointTestDifferent {
   @Test
 
   public void testArea() {
     Point p1 = new Point(-1, 0);
-    Point p2 = new Point(-1, 0);
-
-    Assert.assertEquals(p1.distance(p2), 12.041594578792296);
-
-    p1 = new Point(-1, 0);
-    p2 = new Point(-3, -5);
+    Point p2 = new Point(-3, -5);
 
     Assert.assertEquals(p1.distance(p2), 5.385164807134504);
   }
